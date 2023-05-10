428582382511821ba96e0f466d2e15a757262249
John Sebastian <villegassebastian236@gmail.com>
2023-05-10 18:28:03 -0500

    9 commit

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,5 @@
 import gmpy2
+import math
 
 
 def suma_modulo(a, b, n):
@@ -13,19 +14,14 @@ def multiplicacion_modulo(a, b, n):
     return gmpy2.f_mod(a * b, n)
 
 
-def division_modular(dividendo, divisor, modulo):
-        try:
-            a = gmpy2.mpz(dividendo)
-            b = gmpy2.mpz(divisor)
-            c = gmpy2.mpz(modulo)
-
-            resultado = gmpy2.f_mod(a // b, c)
-
-            return resultado
-
-        except (ValueError, gmpy2.DivisionByZero):
-            print("Error: valores no válidos para la división")
-            return None
+def division_modular(a,b,n):
+    try:
+        inv = pow(b,-1, n)
+        print(inv)
+        if inv != 0:
+            return ((a * inv) % n)
+    except (ValueError, ZeroDivisionError):
+        return "no se puede operar"
 
 
 def potencia_modular(a, b, n):
@@ -33,7 +29,7 @@ def potencia_modular(a, b, n):
     return gmpy2.powmod(a, b, n)
 
 
-def inversos_multiplicativos_modulo( n):
+def inversos_multiplicativos_modulo(n):
     inversos = []
     for i in range(n):
         if gmpy2.gcd(i, n) == 1:
@@ -42,38 +38,24 @@ def inversos_multiplicativos_modulo( n):
     return inversos
 
 
-def raices_cuadradas_modulares(a, n):
-    try:
-        a = gmpy2.mpz(a)
-        n = gmpy2.mpz(n)
+def raices_cuadradas_modulares(n):
+    raices = []
+    for i in range(n // 2, 2, -1):
+        resto = n % i
+        if resto == 0:
+            aux = n // i
+            if aux == 2 or (aux % 2 != 0 and aux != n and aux != 1):
+                raices.append(aux)
+    return raices
 
-        if gmpy2.legendre(a, n) != 1:
-            print("Error: a no es un residuo cuadrático módulo n")
-            return None
 
-        raices = []
-
-        raiz = gmpy2.powmod(a, (n + 1) // 4, n)
-
-        if gmpy2.f_mod(gmpy2.square(raiz), n) == a:
-            raices.append(raiz)
-
-            if n != 1:
-                raices.append(n - raiz)
-
-        return raices
-
-    except ValueError:
-        print("Error: valores no válidos para calcular las raíces cuadradas modulares")
-        return None
-
-def cuadrados_perfectos_modulo(a, n):
-    cuadrados_perfectos = []
-    for i in range(n):
-        cuadrado = gmpy2.powmod(i, 2, n)
-        if cuadrado == a % n:
-            cuadrados_perfectos.append(i)
-    return cuadrados_perfectos
+def cuadrados_perfectos_modulo(n):
+        cuadrados_perfectos = []
+        for i in range(int(math.sqrt(n)) + 1):
+            cuadrado = i * i
+            if cuadrado <= n:
+                cuadrados_perfectos.append(cuadrado)
+        return cuadrados_perfectos
 
 
 def main():
@@ -155,8 +137,8 @@ def main():
                     b = b
                 else:
                     b = abs(b)
-                resultado = division_modular(a, b, n)
-                print("DivisonM=", resultado)
+                print(f"DivisonM= {division_modular(a, b, n)}")
+
             elif eleccion == 4:
                 print("Ingresa un número entero positivo (n)")
                 n = gmpy2.mpz(input())
@@ -187,7 +169,7 @@ def main():
                     n = gmpy2.mpz(input())
                 resultado_a = inversos_multiplicativos_modulo(n)
                 print("InvM=", resultado_a)
-                print(len(resultado_a))
+                print(f"la cantidad es: {len(resultado_a)}")
 
 
 
@@ -197,26 +179,15 @@ def main():
                 while n <= 0:
                     print("Ingresa un número entero positivo (n)")
                     n = gmpy2.mpz(input())
-                print("Ingresa un número de Zn (a)")
-                a = gmpy2.mpz(input())
-                a = a % n
-                if a >= 0:
-                    a = a
-                else:
-                    a = abs(a)
-                resultado_a = raices_cuadradas_modulares(a,n)
-                print("Las raíces cuadradas modulares en n de a son:", resultado_a)
+                resultado = raices_cuadradas_modulares(n)
+                print("Las raíces cuadradas modulares en n son:", resultado)
             elif eleccion == 7:
                 print("Ingresa un número entero positivo (n)")
                 n = gmpy2.mpz(input())
                 while n <= 0:
                     print("Ingresa un número entero positivo (n)")
                     n = gmpy2.mpz(input())
-                print("Ingresa un número de Zn (a)")
-                a = gmpy2.mpz(input())
-                a = a % n
-
-                resultado = cuadrados_perfectos_modulo(a,n)
+                resultado = cuadrados_perfectos_modulo(n)
                 if len(resultado) > 0:
                     print("CuadradosPerfectosM=", resultado)
                     print("La cantidad de cuadrados perfectos=", len(resultado))
